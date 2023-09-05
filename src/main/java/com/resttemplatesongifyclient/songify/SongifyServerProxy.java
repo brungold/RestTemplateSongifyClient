5a7bba971ac9136c26116bb751aa6f8bb0b31b64
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-09-06 00:29:32 +0200

    added @Value annotation method makeGetAllSongsRequest and fill application.properties

--- src/main/java/com/resttemplatesongifyclient/songify/SongifyServerProxy.java
+++ src/main/java/com/resttemplatesongifyclient/songify/SongifyServerProxy.java
@@ -1,9 +1,51 @@
 package com.resttemplatesongifyclient.songify;
 
+import lombok.Value;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @Log4j2
 public class SongifyServerProxy {
+
+    private final RestTemplate restTemplate;
+
+    public SongifyServerProxy(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${songify-server.service.url}")
+    String url;
+
+    @Value("${songify-server.service.port}")
+    int port;
+
+    public SongifyResponse makeGetAllSongsRequest() {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host(url)
+                .port(port)
+                .path("/songs");
+        try {
+            ResponseEntity<SongifyResponse>response = restTemplate.exchange(
+                    builder.build().toUri(),
+                    HttpMethod.GET,
+                    null,
+                    SongifyResponse.class
+            );
+            return response.getBody();
+        } catch (RestClientResponseException exception) {
+            log.error(exception.getStatusText() + " " + exception.getStatusCode().value());
+        } catch (RestClientException exception) {
+            log.error(exception.getMessage());
+        }
+        return null;
+    }
 }
