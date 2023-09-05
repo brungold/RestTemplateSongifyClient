6262edf35bceeb3f172c7eeca9a2f54ea70ca57a
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-09-05 15:43:03 +0200

    first commit

---
+++ src/main/java/com/resttemplatesongifyclient/RestTemplateSongifyClientApplication.java
@@ -0,0 +1,13 @@
+package com.resttemplatesongifyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateSongifyClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateSongifyClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resttemplatesongifyclient/RestTemplateSongifyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resttemplatesongifyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateSongifyClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
