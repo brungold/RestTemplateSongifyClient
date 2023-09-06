3c8f4006660a804dbd9f063b9d4ea422c482f16f
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-09-06 21:46:55 +0200

    refactored method to SongifyRequest from songName to name

--- src/main/java/com/resttemplatesongifyclient/songify/proxy/SongifyRequest.java
+++ src/main/java/com/resttemplatesongifyclient/songify/proxy/SongifyRequest.java
@@ -3,5 +3,5 @@ package com.resttemplatesongifyclient.songify.proxy;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SongifyRequest(String songName, String artist) {
+public record SongifyRequest(String name, String artist) {
 }
